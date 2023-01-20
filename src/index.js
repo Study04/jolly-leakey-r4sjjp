2e523fb653dc079dcc3c9e9365fa1c853fc303be
Study04 <115760750+Study04@users.noreply.github.com>
2023-01-20 00:10:44 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+
+function Main(props) {
+  const [image, setImage] = useState("");
+
+  // write code below this line
+  useEffect(() => {
+    (async function getImage() {
+      const res = await fetch("https://dog.ceo/api/breeds/image/random");
+      const json = await res.json();
+      const imageURL = json["message"];
+      setImage(imageURL);
+    })();
+  }, []);
+  // write code above this line
+
+  if (image === "") return <h1>Waiting for image...</h1>;
+  else return <img src={image} />;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Main />);
